46b84adf64c7fcea65c9c317ea6a28f09078e4ad
TakumaMiwa <takuma.code@gmail.com>
2021-03-05 20:40:36 +0900

    add/bubble_sort0

--- static/js/algoview.js
+++ static/js/algoview.js
@@ -35,7 +35,7 @@ let InputForm = {
         <div class="input-form" id="input-div">
             <input type="number" name="number" id="input-number" v-model.number="card_value">
             <button v-on:click="addCard">Add Card</button>
-            <button v-on:click="exchangeCard">Exchange</button>
+            <button v-on:click="exchangeCard">Bubble Sort</button>
         </div>
         `
 }
@@ -57,7 +57,7 @@ let CardTable = {
             <div class="card-table" id="card-table">
                 <h1> table </h1>
                 <transition-group name="flip-list" tag="div">
-                    <div v-for="item of sortedItemsByPosition" :key="item.id" class="card">
+                    <div v-for="item of cards" :key="item.id" class="card">
                         <h3>{{ item.value }}</h3>
                         <RemoveButton @removeCard="removeCard" card_id="item.id"/>
                     </div>
@@ -76,17 +76,10 @@ let CardTable = {
             type: Array,
             default: []
         }
-    },
-    computed: {
-        sortedItemsByPosition() {
-            return this.cards.sort((a, b) => {
-                return (a.position < b.position) ? -1 : (a.position > b.position) ? 1 : 0;
-            });;
-        }
     }
 }
 let ParentTable = {
-    template: '<div class="app"><input-form :cards="cards" @addCard="cards = $event" @exchangeCard="exchangeCard" /><table-design :cards="cards" @removeCard="removeCard"/></div>',
+    template: '<div class="app"><input-form :cards="cards" @addCard="cards = $event" @exchangeCard="bubbleSort" /><table-design :cards="cards" @removeCard="removeCard"/></div>',
     components: {
         'input-form': InputForm,
         'table-design': CardTable
@@ -105,10 +98,23 @@ let ParentTable = {
             }
             this.cards.splice(index, 1);
         },
-        exchangeCard: function () {
-            let a = this.cards[0].position;
-            this.cards[0].position = this.cards[2].position;
-            this.cards[2].position = a;
+        exchangeCard: function (a, b) {
+            let card = this.cards[a];
+            this.cards.splice(a, 1, this.cards[b]);
+            this.cards.splice(b, 1, card);
+        },
+        bubbleSort: function () {
+            for (let i=0;i<(this.cards.length-1);i++){
+                for(let j=(this.cards.length-1);j>i;j--){
+                    if (this.compareValue(j-1, j)){
+                        this.exchangeCard(j-1, j);
+                    }
+                }
+            }
+        },
+        compareValue: function (a, b) {
+            if (this.cards[a]['value']>this.cards[b]['value'])return true;
+            else return false;
         }
     }
 }
